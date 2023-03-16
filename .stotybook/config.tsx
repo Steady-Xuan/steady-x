4ed600194b25ce6f9600618242b81d394d97c21b
lixuan <2536508277@qq.com>
2023-03-16 14:23:06 +0800

    step 3

---
+++ .stotybook/config.tsx
@@ -0,0 +1,35 @@
+import { configure, addDecorator, addParameters } from "@storybook/react";
+
+import { withInfo } from "@storybook/addon-info";
+import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-regular-svg-icons";
+import "../src/styles/index.scss";
+library.add(fas);
+
+const wrapperStyle: React.CSSProperties = {
+  padding: "20px 40px",
+};
+
+const storyWrapper = (stroyFn: any) => {
+  <div style={wrapperStyle}>
+    <h3>组件演示</h3>
+    {stroyFn()}
+  </div>;
+};
+
+addDecorator(storyWrapper);
+addDecorator(withInfo);
+
+addParameters({ info: { inline: true, header: false } });
+
+const loadderFn = () => {
+  const allExports = [require("../src/welcome.stories.tsx")];
+  const req = require.context("../src/components", true, /\.stories\.tsx$/);
+  req.keys().forEach((fname) => {
+    allExports.push(req(fname));
+  });
+  return allExports;
+};
+
+configure(loadderFn, module);
